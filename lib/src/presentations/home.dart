76954b8ecbc52b2e756fc880530030ce64b20b73
Bogdan M <bogdancons99@gmail.com>
2021-01-10 14:41:41 +0200

    add firebase services

--- lib/src/presentations/home.dart
+++ lib/src/presentations/home.dart
@@ -3,10 +3,6 @@ import 'package:flutter/material.dart';
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
-      ),
-    );
+    return Container();
   }
 }
