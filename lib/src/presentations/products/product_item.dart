8afced7d1f82a523db75e32f4a7d56c7746de545
Bogdan M <bogdancons99@gmail.com>
2021-01-24 22:25:51 +0200

    Update product_item.dart

--- lib/src/presentations/products/product_item.dart
+++ lib/src/presentations/products/product_item.dart
@@ -38,7 +38,7 @@ class ProductItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
+        //crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           Image.network(
             product.images[0],
@@ -47,87 +47,93 @@ class ProductItem extends StatelessWidget {
             height: 200,
           ),
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: <Widget>[
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Container(width: 150, child: Text(product.title)),
-                      const Icon(Icons.favorite),
-                    ],
-                  ),
-                  Row(
-                    children: <Widget>[
-                      Row(
-                        children: List<Widget>.generate(
-                          5,
-                          (int index) {
-                            if (index < product.review.round())
+            child: SizedBox(
+              height: 200,
+              child: Padding(
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Container(
+                          width: 150,
+                          child: Text(
+                            product.title,
+                            maxLines: 3,
+                          ),
+                        ),
+                        const Icon(Icons.favorite),
+                      ],
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Row(
+                          children: List<Widget>.generate(
+                            5,
+                            (int index) {
+                              if (index < product.review.round())
+                                return const Icon(
+                                  Icons.star,
+                                  color: Colors.yellow,
+                                );
                               return const Icon(
-                                Icons.star,
-                                color: Colors.yellow,
+                                Icons.star_border,
                               );
-                            return const Icon(
-                              Icons.star_border,
-                            );
-                          },
+                            },
+                          ),
                         ),
-                      ),
-                      Text('${product.review}'),
-                    ],
-                  ),
-                  Row(
-                    children: <Widget>[
-                      Icon(
-                        Icons.circle,
-                        color: _getColor(product.productState),
-                        size: 16,
-                      ),
-                      _getState(product.productState),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 32,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Text(
-                        '${product.price}',
-                        style: const TextStyle(
-                          color: Colors.red,
-                          fontSize: 20,
+                        Text('${product.review}'),
+                      ],
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Icon(
+                          Icons.circle,
+                          color: _getColor(product.productState),
+                          size: 16,
                         ),
-                      ),
-                      Stack(
-                        alignment: AlignmentDirectional.center,
-                        children: <Widget>[
-                          Container(
-                            height: 40,
-                            width: 40,
-                            decoration: const BoxDecoration(
-                              color: Colors.blue,
-                              gradient: LinearGradient(
-                                colors: <Color>[Colors.blue, Colors.lightBlueAccent],
-                                begin: Alignment.centerLeft,
-                                end: Alignment.centerRight,
+                        _getState(product.productState),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Text(
+                          '${product.price}',
+                          style: const TextStyle(
+                            color: Colors.red,
+                            fontSize: 20,
+                          ),
+                        ),
+                        Stack(
+                          alignment: AlignmentDirectional.center,
+                          children: <Widget>[
+                            Container(
+                              height: 40,
+                              width: 40,
+                              decoration: const BoxDecoration(
+                                color: Colors.blue,
+                                gradient: LinearGradient(
+                                  colors: <Color>[Colors.blue, Colors.lightBlueAccent],
+                                  begin: Alignment.centerLeft,
+                                  end: Alignment.centerRight,
+                                ),
+                                borderRadius: BorderRadius.all(Radius.circular(12)),
                               ),
-                              borderRadius: BorderRadius.all(Radius.circular(12)),
                             ),
-                          ),
-                          const Icon(
-                            Icons.add_shopping_cart,
-                            color: Colors.white,
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ],
+                            const Icon(
+                              Icons.add_shopping_cart,
+                              color: Colors.white,
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
           )
