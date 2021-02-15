aa63815c92c96473fe6d23e8a49323bc08b3a923
Bogdan M <bogdancons99@gmail.com>
2021-02-15 22:32:20 +0200

    safety checkout condition to continue

--- lib/src/presentations/cart/checkout/checkout_page.dart
+++ lib/src/presentations/cart/checkout/checkout_page.dart
@@ -1,3 +1,4 @@
+import 'package:emag_clone/src/containers/auth/checkout_address_container.dart';
 import 'package:emag_clone/src/containers/auth/index.dart';
 import 'package:emag_clone/src/models/auth/index.dart';
 import 'package:emag_clone/src/presentations/app_routes.dart';
@@ -11,105 +12,127 @@ class CheckoutPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CartContainer(
-      builder: (BuildContext context, Cart cart) => Scaffold(
-        appBar: AppBar(
-          iconTheme: const IconThemeData().copyWith(color: Colors.blue),
-          title: const Text(
-            'Checkout',
-            style: TextStyle(color: Colors.black),
+      builder: (BuildContext context, Cart cart) {
+        return CheckoutAddressContainer(
+        builder: (BuildContext context, AddressPoint addressPoint) {
+          return Scaffold(
+          appBar: AppBar(
+            iconTheme: const IconThemeData().copyWith(color: Colors.blue),
+            title: const Text(
+              'Checkout',
+              style: TextStyle(color: Colors.black),
+            ),
+            elevation: 0,
+            backgroundColor: Colors.transparent,
           ),
-          elevation: 0,
-          backgroundColor: Colors.transparent,
-        ),
-        body: DefaultTabController(
-          length: 2,
-          child: Column(
-            children: <Widget>[
-              const TabBar(
-                unselectedLabelColor: Colors.black,
-                labelColor: Colors.blue,
-                tabs: <Widget>[
-                  Tab(
-                    text: 'Home delivery',
-                  ),
-                  Tab(
-                    text: 'Personal pick-up',
-                  ),
-                ],
-              ),
-              Expanded(
-                child: TabBarView(
-                  children: <Widget>[
-                    HomeDeliveryTab(),
-                    PersonalPickupTab(),
+          body: DefaultTabController(
+            length: 2,
+            child: Column(
+              children: <Widget>[
+                const TabBar(
+                  unselectedLabelColor: Colors.black,
+                  labelColor: Colors.blue,
+                  tabs: <Widget>[
+                    Tab(
+                      text: 'Home delivery',
+                    ),
+                    Tab(
+                      text: 'Personal pick-up',
+                    ),
                   ],
                 ),
-              ),
-              const Divider(),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: <Widget>[
-                  const Text(
-                    'Total:',
-                    style: TextStyle(
-                      color: Colors.blue,
-                      fontWeight: FontWeight.bold,
-                    ),
+                Expanded(
+                  child: TabBarView(
+                    children: <Widget>[
+                      HomeDeliveryTab(),
+                      PersonalPickupTab(),
+                    ],
                   ),
-                  Text(
-                    '${cart.totalAmount.toStringAsFixed(2)} lei',
-                    style: const TextStyle(
-                      color: Colors.red,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
-              ),
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Stack(
+                ),
+                const Divider(),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: <Widget>[
-                    RaisedButton(
-                      color: Colors.blueAccent,
-                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
-                      child: Container(
-                        width: double.infinity,
-                        child: const Center(
-                          child: Padding(
-                            padding: EdgeInsets.all(12.0),
-                            child: Text(
-                              'CONTINUE',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 20,
+                    const Text(
+                      'Total:',
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      '${cart.totalAmount.toStringAsFixed(2)} lei',
+                      style: const TextStyle(
+                        color: Colors.red,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Stack(
+                    children: <Widget>[
+                      RaisedButton(
+                        color: Colors.blueAccent,
+                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+                        child: Container(
+                          width: double.infinity,
+                          child: const Center(
+                            child: Padding(
+                              padding: EdgeInsets.all(12.0),
+                              child: Text(
+                                'CONTINUE',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                ),
                               ),
                             ),
                           ),
                         ),
+                        onPressed: () {
+                          if (addressPoint != null) {
+                            Navigator.pushNamed(context, AppRoutes.paymentPage);
+                          } else {
+                            showDialog<AlertDialog>(
+                              context: context,
+                              builder: (BuildContext context) => AlertDialog(
+                                title: const Text('Please enter an address'),
+                                actions: <Widget>[
+                                  FlatButton(
+                                      onPressed: () {
+                                        Navigator.pop(context);
+                                      },
+                                      child: const Text('CLOSE'))
+                                ],
+                              ),
+                            );
+                          }
+                        },
                       ),
-                      onPressed: () {
-                        Navigator.pushNamed(context, AppRoutes.paymentPage);
-                      },
-                    ),
-                    Container(
-                      decoration: const BoxDecoration(
-                        color: Colors.red,
-                        borderRadius: BorderRadius.horizontal(left: Radius.circular(12)),
+                      Container(
+                        decoration: const BoxDecoration(
+                          color: Colors.red,
+                          borderRadius: BorderRadius.horizontal(left: Radius.circular(12)),
+                        ),
+                        height: 48,
+                        width: 30,
+                        child: const Icon(
+                          Icons.arrow_forward,
+                          color: Colors.white,
+                        ),
                       ),
-                      height: 48,
-                      width: 30,
-                      child: const Icon(
-                        Icons.arrow_forward,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
-        ),
-      ),
+        );
+        },
+      );
+      },
     );
   }
 }
